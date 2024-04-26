61e3f450137137dc71c2494ae044432a6235c9fc
Styn Vercauteren <stynvercauteren@MacBook-Pro-van-Styn.local>
2024-04-26 08:56:50 +0200

    fix title on homepage

--- app/(site)/[lang]/page.tsx
+++ app/(site)/[lang]/page.tsx
@@ -5,12 +5,14 @@ import { PortableText } from "@portabletext/react";
 import Link from "next/link";
 
 export default async function Home({ params }: { params: { lang: string } }) {
-  const posts: Post[] = await getPosts();
   const { lang } = params
+  const posts: Post[] = await getPosts(lang);
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-white text-black">
       {posts.map(post => {
+        console.log(post.title)
+
         return (
           <article key={post._id}>
             <Link href={`${lang}/posts/${post.slug.current}`}>
--- sanity/sanity.query.ts
+++ sanity/sanity.query.ts
@@ -1,12 +1,12 @@
 import { groq } from "next-sanity";
 import client from "./sanity.client";
 
-export async function getPosts() {
+export async function getPosts(lang: string) {
   return client.fetch(
     groq`*[_type == "post"]{
       _id,
       _type,
-      title,
+      "title": title[_key == "${lang}"][0].value,
       slug,
       mainImage {
         alt, "image": asset->url
